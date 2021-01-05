da27a98e0b59af388f2e43034e23e4bd0eac6b21
nicolas lequeux <contact@nicolaslequeux.com>
2021-01-05 10:55:21 +0100

    pause dev

--- s02-mini-microservices-app/blog/posts/index.js
+++ s02-mini-microservices-app/blog/posts/index.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const { randomBytes } = require("crypto");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,17 +9,22 @@ app.use(cors());
 
 const posts = {};
 
-app.get("/posts", (req, res) => {
-  console.log("GET request");
+app.get('/posts', (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {
-  console.log("POST request");
-  const id = randomBytes(4).toString("hex");
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex');
   const { title } = req.body;
-  posts[id] = { id, title };
+
+  posts[id] = {
+    id,
+    title
+  };
+
   res.status(201).send(posts[id]);
 });
 
-app.listen(4000, () => console.log("Listening port 4000"));
+app.listen(4000, () => {
+  console.log('Listening on 4000');
+});
